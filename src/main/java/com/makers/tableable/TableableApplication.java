3f1da28dffb378049fc4e18150e0ca38cbf24c76
Curaca <curaca492@gmail.com>
2023-05-08 08:03:19 -0500

    Domain-oriented architecture established by packages, and dependicies management with gradle.

---
+++ src/main/java/com/makers/tableable/TableableApplication.java
@@ -0,0 +1,13 @@
+package com.makers.tableable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableableApplication.class, args);
+    }
+
+}
