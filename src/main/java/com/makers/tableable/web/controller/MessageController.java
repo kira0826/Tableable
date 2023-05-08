1ff50c26e0960f4831b06fc27c745c6068fe9a86
Curaca <curaca492@gmail.com>
2023-05-08 17:20:10 -0500

    API Rest messages controller added.

--- src/main/java/com/makers/tableable/web/controller/MessageController.java
+++ src/main/java/com/makers/tableable/web/controller/MessageController.java
@@ -1,9 +1,11 @@
 package com.makers.tableable.web.controller;
 
+import com.makers.tableable.domain.MessageDTO;
 import com.makers.tableable.domain.service.MessageDTOService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/messages")
@@ -14,6 +16,30 @@ public class MessageController {
     @Autowired
     public MessageController(MessageDTOService messageDTOService) {
         this.messageDTOService = messageDTOService;
+    }
+
+    @GetMapping("/all")
+    public List<MessageDTO> getAll(){
+        return messageDTOService.getAll();
+    }
+
+    @GetMapping("/greatMessages")
+
+    public Optional<List<MessageDTO>> getGreatMessages(){
+        return messageDTOService.messagesWithGreatSatisfaction();
+    }
+    @GetMapping("/badMessages")
+
+    public Optional<List<MessageDTO>> getBadMessages(){
+        return messageDTOService.messagesWithBadSatisfaction();
+    }
+    @PostMapping("/saveMessage")
+    public MessageDTO save(MessageDTO message){
+        return messageDTOService.save(message);
+    }
+    @DeleteMapping("/deleteMessage")
+    public void delete(int key){
+        messageDTOService.delete(key);
     }
 
 }
